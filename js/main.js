726c28345dacc97983012b77469a629a364189af
a.pozdnyakov <a.pozdnyakov@itransition.com>
2018-07-23 12:02:06 +0300

    tmp commit

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 const app = new PIXI.Application({
     width: 1024,
-    height: 1024
+    height: 656
 });
 const direction = {
     down: 0,
@@ -18,18 +18,22 @@ document.body.appendChild(app.view);
 
 const moveMixin = {
     moveDown: function() {
+        this.direction = 'down';
         this.vy = 5;
         this.moveCycle(direction.down);
     },
     moveLeft: function() {
+        this.direction = 'left';
         this.vx = -5;
         this.moveCycle(direction.left);
     },
     moveRight: function() {
+        this.direction = 'right';
         this.vx = 5;
         this.moveCycle(direction.right);
     },
     moveUp: function() {
+        this.direction = 'up';
         this.vy = -5;
         this.moveCycle(direction.up);
     },
@@ -45,47 +49,61 @@ const moveMixin = {
     }
 };
 
-function Hero(texture) {
+function Hero(texture, texture1) {
     this.sx = 48;
     this.sy = 64;
     this.vx = 0;
     this.vy = 0;
+    this.direction = 'down';
     this.timerMove = 100;
 
     this.animationCycle = 4;
     let rectangle = new PIXI.Rectangle(0, 0, this.sx, this.sy);
     texture.frame = rectangle;
     this.sprite = new PIXI.Sprite(texture);
+    this.animationHandler(texture, texture1);
     app.stage.addChild(this.sprite);
 }
+
+Hero.prototype.animationHandler = function(texture, texture1) {
+    let frames = [texture, texture1];
+    /*for (let i = 0; i < this.animationCycle; i++) {
+
+        frames.push(new PIXI.Rectangle(this.sx * i, this.sy * 0 * direction[this.direction], this.sx, this.sy));
+    }*/
+    let anim = new PIXI.extras.AnimatedSprite(frames);
+    anim.x = app.screen.width / 4;
+    anim.y = app.screen.height / 4;
+    anim.anchor.set(0.5);
+    anim.animationSpeed = 0.5;
+    anim.play();
+    app.stage.addChild(anim);
+    app.ticker.add(function(){
+        anim.rotation += 0.01;
+    })
+};
 
 for (let key in moveMixin) {
     Hero.prototype[key] = moveMixin[key];
 }
 
-PIXI.loader.add('ryuk', 'img/heroes/ryuk.png').load((loader, resources) => {
+PIXI.loader.add([{name: 'ryuk', url: 'img/heroes/ryuk.png'}, {name: 'bunny', url: 'img/bunny.png'}]).load((loader, resources) => {
 
+    console.log('resources', resources);
     const texture = resources['ryuk'].texture;
-    createHeroes(texture);
+    const texture1 = resources['bunny'].texture;
+    createHeroes(texture, texture1);
     eventHandler();
-    app.ticker.add(delta => gameLoop(delta));
+    // app.ticker.add(delta => gameLoop(delta));
 });
 
 function gameLoop(delta) {
     heroes.ryuk.sprite.x += heroes.ryuk.vx;
     heroes.ryuk.sprite.y += heroes.ryuk.vy;
-    for (let i = 0; i < this.animationCycle; i++) {
-        if (heroes.ryuk.vx && heroes.ryuk.vy) {
-            requestAnimationFrame(() => {
-                heroes.ryuk.sprite['_texture'].frame =
-                    new PIXI.Rectangle(heroes.ryuk.sx * i%4, heroes.ryuk.sy * 0, heroes.ryuk.sx, heroes.ryuk.sy);
-            });
-        }
-    }
 }
 
-function createHeroes(texture) {
-    heroes.ryuk = new Hero(texture);
+function createHeroes(texture, texture1) {
+    heroes.ryuk = new Hero(texture, texture1);
 }
 
 function eventHandler() {
